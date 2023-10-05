7807ed7e582af3b95999957841636c99f1f52b80
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-10-05 15:33:50 -0700

    argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     arg = sys.argv
     num_arguments = len(arg) - 1
     if num_arguments > 1:
-        print("{:d} arguments.".format(num_arguments))
+        print("{} arguments.".format(num_arguments))
         for i in range(1, num_arguments + 1):
             print("{}: {}".format(i, arg[i]))
     elif num_arguments == 0:
         print("{} arguments.".format(num_arguments))
     else:
         print("{} argument:".format(num_arguments))
-        print("{}: {}".format(num_arguments, arg[i]))
+        print("{}: {}".format(num_arguments, arg[1]))
