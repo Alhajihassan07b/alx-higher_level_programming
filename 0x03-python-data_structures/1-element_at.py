ef3118c31f6a09d9aed5814f135218f5672dec22
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-10-07 06:16:09 -0700

    my list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if idx < 0:
+        if idx <= 0:
             return None
         if idx > len(my_list):
             return None
