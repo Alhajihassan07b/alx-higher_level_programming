b4b7b017e8e6900f61a04550ba3f32e6ddc25031
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-10-08 05:11:25 -0700

    length of sentence

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = ()
+    if len(sentence) == 0:
+        sentence[0] = None
+    else:
+        length = len(sentence), sentence[0]
+    return length
