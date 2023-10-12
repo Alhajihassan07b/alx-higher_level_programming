abdb2c10278f58bd6c5060ab1ae68d1359b5be64
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-10-12 03:28:55 -0700

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,7 +2,10 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = []
 
-    for line in matrix:
-        new_matrix.append([x**2 for x in line])
+    for row in matrix:
+        new_row = []
+        for value in row:
+            new_row.append(value ** 2)
+        new_matrix.append(new_row)
 
-        return (new_matrix)
+    return (new_matrix)
