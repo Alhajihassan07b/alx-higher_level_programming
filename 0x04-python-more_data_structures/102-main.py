ff6c69e8a73980122da6fccbcc6df190271d7cc7
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-10-12 03:43:14 -0700

    complex delete

--- 0x04-python-more_data_structures/102-main.py
+++ 0x04-python-more_data_structures/102-main.py
@@ -1,4 +1,5 @@
-mport__('102-complex_delete').complex_delete
+#!/usr/bin/python3
+complex_delete = __import__('102-complex_delete').complex_delete
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
