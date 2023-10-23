2123d4d14d12d5674d66da33d01e89ac5ef6844e
Hassan Adamu Ibrahim <alhajihassan07b@gmail.com>
2023-10-23 09:00:45 -0700

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
                 count += 1
         print()
         return (count)
-    except IndexError as e:
-        print(f"Error: {e}")
+    except IndexError:
+        print("")
         return (count)
